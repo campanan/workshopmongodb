e6a90cca485556fdde78fe9a88266b0f97aba0f1
Angelo Campana Neto <netocampana@gmail.com>
2022-04-08 11:51:26 -0300

    Added findById method, and exception handling to ObjectNotFoundExceptions

--- src/main/java/com/netocampana/workshopmongodb/controller/UserController.java
+++ src/main/java/com/netocampana/workshopmongodb/controller/UserController.java
@@ -4,9 +4,11 @@ package com.netocampana.workshopmongodb.controller;
 import com.netocampana.workshopmongodb.dto.UserDTO;
 import com.netocampana.workshopmongodb.entities.User;
 import com.netocampana.workshopmongodb.services.UserService;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,5 +32,11 @@ public class UserController {
         return ResponseEntity.ok().body(listDto);
     }
 
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable String id){
+
+        User user = userService.findById(id);
+        return ResponseEntity.ok().body(new UserDTO(user));
+    }
 
 }
--- src/main/java/com/netocampana/workshopmongodb/services/UserService.java
+++ src/main/java/com/netocampana/workshopmongodb/services/UserService.java
@@ -2,10 +2,12 @@ package com.netocampana.workshopmongodb.services;
 
 import com.netocampana.workshopmongodb.entities.User;
 import com.netocampana.workshopmongodb.repositories.UserRepository;
+import com.netocampana.workshopmongodb.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,5 +17,12 @@ public class UserService {
 
     public List<User> findAll(){
         return userRepository.findAll();
+    }
+
+    public User findById(String id){
+        Optional<User> obj = userRepository.findById(id);
+        return obj.orElseThrow(()
+                -> new ObjectNotFoundException("Objecto não encontrado"));
+
     }
 }
