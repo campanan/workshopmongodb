4ec7ef7a8296441f643d48c6b948ecbb05c1a7d7
Angelo Campana Neto <netocampana@gmail.com>
2022-04-17 16:17:50 -0300

    Added posts references in user

--- src/main/java/com/netocampana/workshopmongodb/entities/User.java
+++ src/main/java/com/netocampana/workshopmongodb/entities/User.java
@@ -1,16 +1,18 @@
 package com.netocampana.workshopmongodb.entities;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @NoArgsConstructor
 @AllArgsConstructor
+@RequiredArgsConstructor
 @Data
 @Document(collection = "user")
 public class User implements Serializable {
@@ -20,8 +22,18 @@ public class User implements Serializable {
     @Id
     private String id;
 
+    @NonNull
     private String name;
 
+    @NonNull
     private String email;
 
+    @DBRef(lazy = true)
+    private List<Post> posts = new ArrayList<>();
+
+    public User(String id, @NonNull String name, @NonNull String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
 }
